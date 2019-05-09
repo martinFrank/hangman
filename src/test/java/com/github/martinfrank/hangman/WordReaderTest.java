10ea74da559819ee54d262dda61a886f680f2bb9
martin.frank <martin.frank.private@gmail.com>
2019-05-09 16:05:18 +0200

    fixed test

--- src/test/java/com/github/martinfrank/hangman/WordReaderTest.java
+++ src/test/java/com/github/martinfrank/hangman/WordReaderTest.java
@@ -3,7 +3,9 @@ package com.github.martinfrank.hangman;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
@@ -12,16 +14,24 @@ public class WordReaderTest {
 
     @Test
     public void testProperWordLength() throws IOException {
-        final int length = 8;
+        String wordA = "abcde";
+        String wordF = "fffff";
+        final int length = wordA.length();
 
         File f = File.createTempFile("pre", "suf");
         f.createNewFile();
+        BufferedWriter br = new BufferedWriter(new FileWriter(f));
+        br.write(wordA);
+        br.write(wordF);
+        br.close();
         String fileName = f.getAbsolutePath();
         System.out.println("filename: " + f + " exists?=" + f.exists());
         List<String> words = new WordReader(fileName).readAllWordsWithLength(length);
         Collections.shuffle(words);
         for (String word : words) {
             Assert.assertEquals(length, word.length());
+            Assert.assertTrue(words.contains(wordA));
+            Assert.assertTrue(words.contains(wordF));
         }
         f.deleteOnExit();
     }
