fddb69167310d150f8cbc7b07ef2936cb0ff6014
martin.frank <martin.frank.private@gmail.com>
2019-05-09 16:02:05 +0200

    fixed test

--- src/test/java/com/github/martinfrank/hangman/WordReaderTest.java
+++ src/test/java/com/github/martinfrank/hangman/WordReaderTest.java
@@ -15,14 +15,15 @@ public class WordReaderTest {
         final int length = 8;
 
         File f = File.createTempFile("pre", "suf");
-        String fileName = f.getAbsolutePath() + File.pathSeparator + f.getName();
+        f.createNewFile();
+        String fileName = f.getAbsolutePath();
         System.out.println("filename: " + f + " exists?=" + f.exists());
         List<String> words = new WordReader(fileName).readAllWordsWithLength(length);
         Collections.shuffle(words);
         for (String word : words) {
             Assert.assertEquals(length, word.length());
         }
-
+        f.deleteOnExit();
     }
 
     @Test
